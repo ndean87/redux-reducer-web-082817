b901bffdc3f7280cf2de45105ce966113fac3c3f
ndean87 <ndean87@gmail.com>
2017-11-15 20:57:28 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,22 @@
-export function manageFriends(state, action){
+const defaultState = {
+  friends: [
+    { name: 'Avi', hometown: 'NYC', id: 100 }
+  ]
+}
+
+export function manageFriends(state = defaultState, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+        return {friends: state.friends.concat(action.friend)}
+      break;
+    case 'REMOVE_FRIEND':
+        return {friends: state.friends.filter(friend => {
+          return friend.id !== action.id
+          })
+        }
+      break;
+    default:
+      return state;
+
+  }
 }
